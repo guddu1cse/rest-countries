f21df99d1d2995d0ca64a3fd0b0cec705e55803e
Guddu Kumar <guddu.kumar.31.3@mountblue.tech>
2025-02-21 15:26:07 +0530

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,8 @@ import { useState, useEffect } from "react";
 const SearchBar = ({ dark, countries, setCountries, data, region }) => {
   const [search, setSearch] = useState("");
   const [selectedRegion, setSelectedRegion] = useState("none");
-  const [subRegion, setSubRegion] = useState("none");
+  const [subRegion, setSubRegion] = useState([]);
+  const [selectedSubRegion, setSelectedSubRegion] = useState("none");
   const [sort, setSort] = useState(false);
 
   useEffect(() => {
@@ -14,6 +15,31 @@ const SearchBar = ({ dark, countries, setCountries, data, region }) => {
         : [...prevState].sort((a, b) => b.population - a.population)
     );
   }, [sort]);
+
+  useEffect(() => {
+    if (selectedRegion !== "none") {
+      setSubRegion(() => {
+        return countries.reduce(
+          (acc, currVal) =>
+            acc.includes(currVal.subregion) ? acc : [...acc, currVal.subregion],
+          []
+        );
+      });
+    } else {
+      setSubRegion([]);
+      setSelectedSubRegion("none");
+    }
+  }, [selectedRegion]);
+
+  useEffect(() => {
+    if (selectedSubRegion !== "none") {
+      setCountries(
+        data
+          .filter((country) => country.region === selectedRegion)
+          .filter((country) => country.subregion === selectedSubRegion)
+      );
+    }
+  }, [selectedSubRegion]);
 
   const handleChange = (searchItem) => {
     setSearch(searchItem);
@@ -39,6 +65,11 @@ const SearchBar = ({ dark, countries, setCountries, data, region }) => {
         : data.filter((country) => country.region === e.target.value)
     );
     setSelectedRegion(e.target.value);
+  };
+
+  const handleSubRegionChange = (e) => {
+    setSelectedSubRegion(e.target.value);
+    console.log(e.target.value);
   };
 
   return (
@@ -77,19 +108,39 @@ const SearchBar = ({ dark, countries, setCountries, data, region }) => {
         </button>
       </div>
 
-      <select
-        className={`p-2 px-4 rounded cursor-pointer
+      <div className="sm:flex items-center">
+        {/* region options */}
+        <select
+          className={`p-2 px-4 m-2 rounded cursor-pointer
       ${dark ? "bg-gray-500 text-white" : "bg-gray-200 text-[#1E2939]"}
       `}
-        onChange={(e) => handleResionChange(e)}
-      >
-        <option value="none">Filter by Region</option>
-        {region.map((region) => (
-          <option key={region} value={region}>
-            {region}
-          </option>
-        ))}
-      </select>
+          onChange={(e) => handleResionChange(e)}
+        >
+          <option value="none">Filter by Region</option>
+          {region.map((region) => (
+            <option key={region} value={region}>
+              {region}
+            </option>
+          ))}
+        </select>
+
+        {/* sub region options */}
+        {subRegion.length > 0 && (
+          <select
+            className={`p-2 px-4 rounded cursor-pointer
+        ${dark ? "bg-gray-500 text-white" : "bg-gray-200 text-[#1E2939]"}
+        `}
+            onChange={(e) => handleSubRegionChange(e)}
+          >
+            <option value="none">Filter by Sub Region</option>
+            {subRegion.map((region) => (
+              <option key={region} value={region}>
+                {region}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
     </div>
   );
 };
